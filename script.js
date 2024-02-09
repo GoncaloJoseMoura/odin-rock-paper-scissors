6d89f99011efc1fe8254c88e5a7164995128a100
GoncaloJoseMoura <43656133+GoncaloJoseMoura@users.noreply.github.com>
2024-02-09 23:44:37 +0000

    Make the game a best out of 5

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-const choicesArray = ['Rock', 'Paper', 'Scissor']
-
-let computerChoice = getComputerChoice()
-let playerSelection = prompt('Pick Rock, Paper or Scissors')
+const choicesArray = ['rock', 'paper', 'scissor'];
+let myScore = 0;
+let enemyScore = 0;
 
 function getComputerChoice() {
     return choicesArray[Math.floor(Math.random() * choicesArray.length)];
@@ -10,17 +9,30 @@ function getComputerChoice() {
 
 function gameFirstRound(computerChoice, playerSelection) {
     if (
-        (computerChoice === 'Rock' && playerSelection === 'Paper') ||
-        (computerChoice === 'Scissor' && playerSelection === 'Rock') || 
-        (computerChoice === 'Paper' && playerSelection === 'Scissor')) {
+        (computerChoice === 'rock' && playerSelection === 'paper') ||
+        (computerChoice === 'scissor' && playerSelection === 'rock') || 
+        (computerChoice === 'paper' && playerSelection === 'scissor')) {
+        ++myScore;
         return 'Player Won';
     } else if (computerChoice === playerSelection) {
         return 'It\'s a tie';
     } else {
+        ++enemyScore;
         return 'Computer Won';
+
     }
 }
 
-console.log(`your choice: ${playerSelection}`)
-console.log(`Computer's choice: ${computerChoice}`)
-console.log(gameFirstRound(computerChoice, playerSelection))
+for (let i = 1; i <= 5; ++i) {
+    let computerChoice = getComputerChoice()
+    let playerSelection = prompt('Pick Rock, Paper or Scissors').toLowerCase()
+
+    console.log(`Round number: ${i}`)
+    console.log(`your choice: ${playerSelection}`)
+    console.log(`Computer's choice: ${computerChoice}`)
+    console.log(gameFirstRound(computerChoice, playerSelection))
+    console.log("*".repeat(50))
+
+}
+console.table({'Tie\'s': 5 - enemyScore - myScore ,'Player': myScore, 'Enemy': enemyScore}, ['names','scores'])
+console.log( (myScore > enemyScore) ? 'You Won': myScore == enemyScore? 'It\'s a tie': 'You lost')
