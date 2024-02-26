30d1e5218da120f23dabd43763f91b6b8ebf365b
GoncaloJoseMoura <43656133+GoncaloJoseMoura@users.noreply.github.com>
2024-02-26 17:47:06 +0000

    Add Base Dynamic HTML Changes

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 const choicesArray = ['rock', 'paper', 'scissor'];
 let myScore = 0;
 let enemyScore = 0;
+let round = 1
 
 function getComputerChoice() {
     return choicesArray[Math.floor(Math.random() * choicesArray.length)];
 
 }
 
-function playRound(computerChoice, playerSelection) {
+function playRound(computerChoice, playerChoice) {
     if (
-        (computerChoice === 'rock' && playerSelection === 'paper') ||
-        (computerChoice === 'scissor' && playerSelection === 'rock') || 
-        (computerChoice === 'paper' && playerSelection === 'scissor')) {
+        (computerChoice === 'rock' && playerChoice === 'paper') ||
+        (computerChoice === 'scissor' && playerChoice === 'rock') || 
+        (computerChoice === 'paper' && playerChoice === 'scissor')) {
         ++myScore;
-        return 'Player Won';
-    } else if (computerChoice === playerSelection) {
+        return 'You Won';
+    } else if (computerChoice === playerChoice) {
         return 'It\'s a tie';
     } else {
         ++enemyScore;
@@ -23,16 +24,40 @@ function playRound(computerChoice, playerSelection) {
     }
 }
 
-for (let i = 1; i <= 5; ++i) {
-    let computerChoice = getComputerChoice()
-    let playerSelection = prompt('Pick Rock, Paper or Scissors').toLowerCase()
+const button = document.querySelector('button');
+const para = document.createElement('p')
+const span = document.querySelector('span')
+para.setAttribute('class', 'currentResults')
+button.parentNode.parentNode.appendChild(para)
 
-    console.log(`Round number: ${i}`)
-    console.log(`your choice: ${playerSelection}`)
-    console.log(`Computer's choice: ${computerChoice}`)
-    console.log(playRound(computerChoice, playerSelection))
-    console.log("*".repeat(50))
+button.addEventListener('click', () => {
+    let playerChoice = button.getAttribute('class');
+    let computerChoice = getComputerChoice();
+    console.log(computerChoice);
 
-}
-console.table({'Tie\'s': 5 - enemyScore - myScore ,'Player': myScore, 'Enemy': enemyScore}, ['names','scores'])
-console.log( (myScore > enemyScore) ? 'You Won': myScore == enemyScore? 'It\'s a tie': 'You lost')
+    para.textContent = playRound(computerChoice, playerChoice);
+    span.textContent = ++round;
+
+    document.querySelector('#PlayerScore').textContent = myScore
+    document.querySelector('#ComputerScore').textContent = enemyScore
+
+    if (myScore === 5 || enemyScore == 5) {
+        let winner = myScore === 5? 'You!!!': 'The Computer'
+        para.textContent = 'And the Final Winner is: ' + winner
+
+    }
+});
+
+// for (let i = 1; i <= 5; ++i) {
+//     let computerChoice = getComputerChoice()
+//     let playerSelection = prompt('Pick Rock, Paper or Scissors').toLowerCase()
+
+//     console.log(`Round number: ${i}`)
+//     console.log(`your choice: ${playerSelection}`)
+//     console.log(`Computer's choice: ${computerChoice}`)
+//     console.log(gameFirstRound(computerChoice, playerSelection))
+//     console.log("*".repeat(50))
+
+// }
+// console.table({'Tie\'s': 5 - enemyScore - myScore ,'Player': myScore, 'Enemy': enemyScore}, ['names','scores'])
+// console.log( (myScore > enemyScore) ? 'You Won': myScore == enemyScore? 'It\'s a tie': 'You lost')
